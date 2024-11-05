122a7368e402072116cfa9837bff871878509107
adithya-naik <jatothadithyanaik@gmail.com>
2024-11-05 23:44:48 +0530

    updated-final version

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-let random = Math.random() * 100
 
-console.log(random)
+let random = Math.random() * 10
+
+// console.log(random)
 
 let fault = {
     "+" : "-",
     "*" : "+",
     "-" : "/",
     "/" : "**",
-
 }
 
-let num1 = prompt("Enter First Number : ")
-let num2 = prompt("Enter Second Number : ")
-let Operator = prompt("Enter the Operator : ")
-console.log(`First Number ${num1} and Second Number ${num2} and the Selected operator is ${Operator}`)
-console.log("The result  : ")
-if(random>10){
-    //Calculates correct output
-    console.log(eval(`${num1} ${Operator} ${num2}`))
-    alert("\n\nTrust me...🤝\n I gave you Correct answer")
-}
-else{
-        //Calculates Wrong output
-        Operator = fault[Operator]
-        console.log(eval(`${num1} ${Operator} ${num2}`))
-        alert("\n\nHehe...😂 \n I gave you Wrong answer")
+
+function clicked(){
+    let num1 = document.querySelector('#inputEmail4').value;
+    let num2 = document.querySelector('#inputPassword4').value;
+    let Operator = document.querySelector('#inputAddress').value;
+    let answer = "";
+    let resultText="";
+    let state = true;
+    if(random>7.2){
+        //Calculates correct output
+        answer = eval(`${num1} ${Operator} ${num2}`);
+        resultText = "Trust me...&#129309;\n I gave you Correct answer";
+    }
+    else{
+            //Calculates Wrong output
+            Operator = fault[Operator];
+            answer = eval(`${num1} ${Operator} ${num2}`);
+            resultText = "Hehe...&#128514; \n I gave you Wrong answer";
+            state = false;
+    }
+
+
+// console.log(random);
+    var newAnsHTML =`
+        <h2>Result : ${answer}</h2>
+    `
+    document.querySelector('.answer').innerHTML = newAnsHTML;
+
+    var newMsgHTML =`
+        <h6><b>Message</b> : ${resultText}</h6>
+    `
+    var msgElemet = document.querySelector('p');
+    if(!state){
+        msgElemet.classList.add("msgFalse");
+    }
+    document.querySelector('p').innerHTML = newMsgHTML;
 }
